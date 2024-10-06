10479ab039460692a390426b94b1279d8088e2ce
eziquexx <waftyann@gmail.com>
2024-10-06 19:07:41 +0900

    프로젝트 생성

---
+++ src/main/java/comhellojalbajo/ott/HellojalbajoOttApplication.java
@@ -0,0 +1,13 @@
+package comhellojalbajo.ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellojalbajoOttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellojalbajoOttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comhellojalbajo/ott/HellojalbajoOttApplicationTests.java
@@ -0,0 +1,13 @@
+package comhellojalbajo.ott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojalbajoOttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
